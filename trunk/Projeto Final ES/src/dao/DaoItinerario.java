6ab3e850460deffdc299de39f1570effe185fbab
hanniere@gmail.com <hanniere@gmail.com@99780ea3-a9bd-f5e7-93f2-9a33c1bc0b0f>
2012-07-01 22:48:17 +0000

    Atualizacao da daoItinerario

--- trunk/Projeto Final ES/src/dao/DaoItinerario.java
+++ trunk/Projeto Final ES/src/dao/DaoItinerario.java
@@ -224,7 +224,7 @@ public class DaoItinerario {
                     + " INNER JOIN Rota ON (rotaId = RotaItinerario_RotaId)"
                     + " INNER JOIN Cidade origem ON (origem.cidadeId = Rota_CidadeOrigem)"
                     + " INNER JOIN Cidade destino ON (destino.cidadeId = Rota_CidadeDestino) WHERE RotaItinerario_ItinerarioId ="+itinerario.getId()
-                    + "GROUP BY origem.CidadeNome,destino.CidadeNome ORDER BY RotaItinerarioOrdem;";
+                    + " GROUP BY origem.CidadeNome,destino.CidadeNome ORDER BY RotaItinerarioOrdem;";
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 Rota rota = new Rota();
